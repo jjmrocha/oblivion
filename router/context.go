a07fd47bf1a50a41f2e26e9e955f913f9af73aad
Joaquim Rocha <joaquim.rocha@gympass.com>
2024-06-18 15:00:19 +0100

    Code refactor

--- router/context.go
+++ router/context.go
@@ -1,10 +1,22 @@
 package router
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
 
 type Context struct {
 	Writer  http.ResponseWriter
 	Request *http.Request
+	Start   time.Time
+}
+
+func (c *Context) fullRequestURI() string {
+	return c.Request.Method + " " + c.Request.RequestURI
+}
+
+func (c *Context) duration() time.Duration {
+	return time.Since(c.Start)
 }
 
 func (c *Context) OK(payload any) (*Response, error) {
